26159f5adb900434ce7440ac3dba09bedb548780
shichipeng <shichipengbs@163.com>
2018-11-21 01:36:37 +0800

    'test'

--- src/main/scala/com/meituan/flink/java/Test1.java
+++ src/main/scala/com/meituan/flink/java/Test1.java
@@ -32,8 +32,6 @@ public class Test1 {
         dataStream.print();
         env.execute("window wordCount");
         System.out.println("my first use it");
-
-
     }
 
 
@@ -45,6 +43,4 @@ public class Test1 {
             }
         }
     }
-
-
 }
